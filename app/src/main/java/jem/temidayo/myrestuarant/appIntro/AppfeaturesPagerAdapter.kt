24301b9f615d167423037130a1780f5024eff6e0
Temidayo Jemilehin <jemiilehin@gmail.com>
2021-12-10 18:54:30 +0100

    viewPager implemented for app welcome slides

---
+++ app/src/main/java/jem/temidayo/myrestuarant/appIntro/AppfeaturesPagerAdapter.kt
@@ -0,0 +1,18 @@
+package jem.temidayo.myrestuarant.appIntro
+
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class AppfeaturesPagerAdapter(val items: ArrayList<Fragment>, activity: AppCompatActivity): FragmentStateAdapter(activity) {
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return items[position]
+    }
+
+}
