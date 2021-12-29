5675c704989d13dd2d85135f948f6ee9be514b5c
Temidayo <jemiilehin@gmail.com>
2021-12-29 14:36:25 +0100

    fixed gradle break issue by implementing google service plugin correctly

--- app/src/main/java/jem/temidayo/myrestuarant/appIntro/features/PagerAdapter.kt
+++ app/src/main/java/jem/temidayo/myrestuarant/appIntro/features/PagerAdapter.kt
@@ -4,7 +4,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-    private const val NUM_PAGES = 2
 
 class PagerAdapter(frg: FragmentActivity): FragmentStateAdapter(frg) {
 
