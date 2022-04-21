384487c8cc0152ed33f1167417c345b50cafc244
Temidayo Jemilehin <jemiilehin@gmail.com>
2022-04-21 17:55:29 +0100

    google map activity has been added but map is yet to be completed

--- app/src/main/java/jem/temidayo/myrestuarant/MainActivity.kt
+++ app/src/main/java/jem/temidayo/myrestuarant/MainActivity.kt
@@ -3,16 +3,19 @@ package jem.temidayo.myrestuarant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
+import jem.temidayo.myrestuarant.RestuarantDataObjects.User
 import jem.temidayo.myrestuarant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
-    private var phone: String = ""
+    private lateinit var user: User
 
     override fun onStart() {
         super.onStart()
@@ -23,7 +26,8 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SigninActivity::class.java)
             startActivity(intent)
         } else{
-            phone = user.phoneNumber.toString()
+//            phone = user.phoneNumber.toString()
+            Log.d("userInfo","${user.displayName}")
         }
     }
 
@@ -36,11 +40,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         auth = FirebaseAuth.getInstance()
+        user = User()
 
-        sendUserLoginData(phone)
+        getUserProfile()
     }
 
-    private fun sendUserLoginData(phone: String?) {
-        binding.welcomeText.setText("Hello $phone")
-    }
+   private fun getUserProfile() {
+       val cUser = auth.currentUser
+       if (cUser != null) {
+           val uname = cUser.displayName
+           val email = cUser.email
+           val phone = cUser.phoneNumber
+
+           user.email = email.toString()
+           user.name = uname.toString()
+           user.phone_number = phone.toString()
+
+           Log.d("currentUser", uname.toString())
+       }
+   }
 }
--- app/src/main/java/jem/temidayo/myrestuarant/RestuarantDataObjects/RestuarantsItemModels.kt
+++ app/src/main/java/jem/temidayo/myrestuarant/RestuarantDataObjects/RestuarantsItemModels.kt
@@ -1,19 +1,20 @@
 package jem.temidayo.myrestuarant.RestuarantDataObjects
 
-import androidx.fragment.app.Fragment
+import java.util.Collections.emptyList
+
 
 data class Food(val id: Int, val name: String, var description: String,
                 var price: Int )
 
 data class  Drink(val id: Int, val name: String,var price: Int)
 
-data class Package(val id: Int, val name: String, var price: Int,
-                   var content: String)
+data class Package(var id: Int, var name: String, var price: Int,
+                   var description: String, var content: MutableList<String>)
 
 data class Invoice(val id: Int, val order_number: String, val reference_number: Int,
                 val description: String, val quantity: Int, val price: String,
                 val total_cost: String)
 
-//var pages: ArrayList<Fragment> = arrayListOf(
-//    Features1(), Features2()
-//)
+data class RestuarantAdrress(var restuarantName: String, var email: String, var contact: String, var address: List<String>){
+    constructor(): this("","","", emptyList())
+}
